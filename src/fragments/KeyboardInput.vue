7cb582dab9c10ed04eaf5b4085da83748c9146f1
Lucas Santos <lhs.santoss@gmail.com>
2022-11-23 18:18:12 +0100

    fix: move input to other fragment
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

---
+++ src/fragments/KeyboardInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="text" class="input keyboard-input" name="keyboardInput" id="" :value="state.keyboardInput" @keydown="handleKeyboardPress">
+</template>
+<script setup lang="ts">
+import { inject } from 'vue'
+import { AllowedAlphabet, ALLOWED_ALPHABET, GlobalState } from '../types'
+
+const state = inject<GlobalState>('state')!
+const emit = defineEmits(['keyboard-input'])
+
+  const handleKeyboardPress = (e: Event) => {
+  e.preventDefault()
+  const key = (e as KeyboardEvent).key.toUpperCase()
+  if (!ALLOWED_ALPHABET.includes(key as AllowedAlphabet)) {
+    return false
+  }
+
+  emit('keyboard-input', key)
+  return true
+}
+</script>
+<style scoped>
+</style>
