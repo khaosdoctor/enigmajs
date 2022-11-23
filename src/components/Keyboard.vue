42745e848c343c8eb5458a6e58f832b198df3d64
Lucas Santos <lhs.santoss@gmail.com>
2022-11-23 14:44:38 +0100

    style: keyboard styling
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,11 +1,9 @@
 <template>
-
   <div class="keyboard-row columns has-text-centered" v-for="row in KEYBOARD_ROWS">
     <span v-for="letter in row" class="column keyboard-key-wrapper mb-5">
-      <span :class="`keyboard-letter-${letter} keyboard-key`">{{letter}}</span>
+      <span :class="`keyboard-letter-${letter} keyboard-key ml-6`">{{letter}}</span>
     </span>
   </div>
-
 </template>
 
 <script setup lang="ts">
@@ -21,8 +19,18 @@ import { KEYBOARD_ROWS } from '../types'
   width: 50px;
   height: 50px;
   border-radius: 50%;
-  background-color: rgb(34, 34, 34);
-  color: rgb(155, 155, 155);
+  background: linear-gradient(135deg, #4c4c4c 0%,#595959 12%,#666666 25%,#474747 39%,#2c2c2c 50%,#000000 51%,#111111 60%,#2b2b2b 76%,#1c1c1c 91%,#131313 100%);
+  color: #d0d0d0;
   font-weight: bold;
+  cursor: pointer;
+  border: 3px solid #999;
+}
+
+.keyboard-key:hover {
+  border: 3px solid #bbb;
+}
+
+.keyboard-key:active {
+  border: 5px solid #bbb;
 }
 </style>
