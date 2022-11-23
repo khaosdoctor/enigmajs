aea1a4d0f74ec82b2a3003ed2a2f2299a88d87bc
Lucas Santos <lhs.santoss@gmail.com>
2022-11-23 17:53:21 +0100

    feat: add input and steps
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -7,8 +7,9 @@ export interface GlobalState {
   }
   reflector: Reflector
   plugboard: { [key in AllowedAlphabet]: AllowedAlphabet | '' }
-  input: string
   output: string
+  keyboardInput: string
+  steps: string[]
 }
 
 export type AllowedAlphabet = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z'
