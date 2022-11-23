ae31f1821a8ca6693106244955bed36940ed5ab1
Lucas Santos <lhs.santoss@gmail.com>
2022-11-23 13:02:57 +0100

    feat: initial keyboard implementation
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,15 +1,28 @@
-<script setup lang="ts">
-import { inject } from 'vue'
-import { GlobalState } from '../types'
-
-const state = inject<GlobalState>('state')
-</script>
-
 <template>
-<h1>Keyboard</h1>
-<pre>{{ state }}</pre>
+
+  <div class="keyboard-row columns has-text-centered" v-for="row in KEYBOARD_ROWS">
+    <span v-for="letter in row" class="column keyboard-key-wrapper mb-5">
+      <span :class="`keyboard-letter-${letter} keyboard-key`">{{letter}}</span>
+    </span>
+  </div>
+
 </template>
 
-<style>
+<script setup lang="ts">
+import { KEYBOARD_ROWS } from '../types'
+</script>
 
+<style scoped>
+.keyboard-key {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: rgb(34, 34, 34);
+  color: rgb(155, 155, 155);
+  font-weight: bold;
+}
 </style>
