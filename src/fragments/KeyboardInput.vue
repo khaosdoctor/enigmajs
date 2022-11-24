7f1ff86334b78f363afe32e8f1b8bc23867e9618
Lucas Santos <lhs.santoss@gmail.com>
2022-11-24 20:21:52 +0100

    refactor(keyboardInput): simplify logic
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/fragments/KeyboardInput.vue
+++ src/fragments/KeyboardInput.vue
@@ -3,7 +3,8 @@
 </template>
 <script setup lang="ts">
 import { inject } from 'vue'
-import { AllowedAlphabet, ALLOWED_ALPHABET, GlobalState } from '../types'
+import { GlobalState } from '../types'
+import { isLetter } from '../util'
 
 const state = inject<GlobalState>('state')!
 const emit = defineEmits(['keyboard-input'])
@@ -11,10 +12,7 @@ const emit = defineEmits(['keyboard-input'])
   const handleKeyboardPress = (e: Event) => {
   e.preventDefault()
   const key = (e as KeyboardEvent).key.toUpperCase()
-  if (!ALLOWED_ALPHABET.includes(key as AllowedAlphabet)) {
-    return false
-  }
-
+  if (!isLetter(key)) return false
   emit('keyboard-input', key)
   return true
 }
